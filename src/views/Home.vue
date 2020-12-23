ad8347d8818153b3d15adf3f0f244ca8a31540ed
JAEHO SONG <thruthesky@gmail.com>
2020-12-23 17:51:19 +0900

    update for fireadin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <LoginForm @success="success" @error="error" v-if="!app.user" />
+    <hr />
+    <RegisterForm @success="success" @error="error" v-if="!app.user" />
     <div @click="app.logout" v-if="app.user">
       UID: {{ app.user.uid }}, Logout
     </div>
@@ -11,15 +13,17 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import LoginForm from "@/fire-admin-vue/components/LoginForm.vue";
+import RegisterForm from "@/fire-admin-vue/components/RegisterForm.vue";
 
 @Options({
   components: {
-    LoginForm
+    LoginForm,
+    RegisterForm
   }
 })
 export default class Home extends Vue {
   success() {
-    console.log("login success");
+    console.log("login or register success");
   }
   error(e: any) {
     alert(e.code + " : " + e.message);
