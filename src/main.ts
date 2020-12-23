1b3aed235fcf598f635e69dced4bc9da1e9d3541
JAEHO SONG <thruthesky@gmail.com>
2020-12-23 17:19:22 +0900

    updating temp vue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import firebase from "firebase/app";
+import { firebaseConfig } from "../app.config";
+
+firebase.initializeApp(firebaseConfig);
+import { AppService } from "@/fire-admin-vue/services/app.service";
+
 createApp(App)
+  .mixin({
+    data() {
+      return {
+        app: new AppService()
+      };
+    }
+  })
   .use(store)
   .use(router)
   .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <LoginForm @success="success" @error="error" v-if="!app.user" />
+    <div @click="app.logout" v-if="app.user">
+      UID: {{ app.user.uid }}, Logout
+    </div>
+    <router-link to="/admin" v-if="app.isAdmin">Admin Dashboard</router-link>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import LoginForm from "@/fire-admin-vue/components/LoginForm.vue";
 
 @Options({
   components: {
-    HelloWorld
+    LoginForm
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  success() {
+    console.log("login success");
+  }
+  error(e: any) {
+    alert(e.code + " : " + e.message);
+  }
+}
 </script>
